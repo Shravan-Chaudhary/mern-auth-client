aba57839ae42dcda1eed29f2241ddf346c3b0ae4
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-08-16 09:41:04 +0530

    Add navbar and Homepage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import { Button } from './components/ui/button'
+import Header from "@/components/Header.tsx";
+import HomeScreen from "@/screens/HomeScreen.tsx";
+
+function App() {
+  return (
+    <div >
+     <Header />
+        <HomeScreen />
+    </div>
+  )
+}
+
+export default App
